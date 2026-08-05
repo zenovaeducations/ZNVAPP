a0b5aefe744d28d3988a8388759b4258bb34e21d
zenovaeducations <zenovaeducations@gmail.com>
2026-08-05 08:32:12 +0530

    Create firebase-config.js

---
+++ firebase/firebase-config.js
@@ -0,0 +1,56 @@
+// =====================================
+// ZENOVA EDUCATIONS
+// Firebase Configuration
+// =====================================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAcqxS2SIpMyaY-jYOz0Y-t67Taa04Fy1o",
+
+    authDomain: "zenova-educations.firebaseapp.com",
+
+    projectId: "zenova-educations",
+
+    storageBucket: "zenova-educations.firebasestorage.app",
+
+    messagingSenderId: "66782392979",
+
+    appId: "1:66782392979:web:2e0c7e70b688401b4f5366",
+
+    measurementId: "G-7JL1SFKDHE"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+export {
+
+    app,
+
+    analytics,
+
+    auth,
+
+    db,
+
+    storage
+
+};
