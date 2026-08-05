2c74e401d348f7d341da75cf91a83a0ff7e6f099
zenovaeducations <zenovaeducations@gmail.com>
2026-08-05 08:33:39 +0530

    Add Firestore utility functions and exports

---
+++ firebase/firestore.js
@@ -0,0 +1,47 @@
+// =====================================
+// Firestore Utilities
+// =====================================
+
+import { db } from "./firebase-config.js";
+
+import {
+
+    doc,
+
+    getDoc,
+
+    setDoc,
+
+    updateDoc,
+
+    deleteDoc,
+
+    collection,
+
+    getDocs,
+
+    serverTimestamp
+
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+export {
+
+    db,
+
+    doc,
+
+    getDoc,
+
+    setDoc,
+
+    updateDoc,
+
+    deleteDoc,
+
+    collection,
+
+    getDocs,
+
+    serverTimestamp
+
+};
